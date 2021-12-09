c5d77149391bdf3e7e6bd5a18ca7bedce825e81b
DanielHealyAdv <37441113+DanielHealyAdv@users.noreply.github.com>
2021-12-09 11:37:32 +0000

    'Completed'

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Response } from '@angular/http';
+import { userInfo } from 'os';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { AlbumStorePage } from '../../e2e/app.po';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
 
   _albumUrl: string = '../assets/album.json';
+  album: any;
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
       .map(response => response.json());
   }
 
 }
+function next(next: any, arg1: (json: Object) => void): Observable<Album> {
+  throw new Error('Function not implemented.');
+}
+
